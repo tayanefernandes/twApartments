3c06002df9de6877400df105be327cb0775f1def
Tayane Fernandes <tayane.rmf@gmail.com>
2015-09-30 11:06:50 -0300

    Validating form

--- public/js/controllers/MaintenanceCtrl.js
+++ public/js/controllers/MaintenanceCtrl.js
@@ -1,13 +1,19 @@
-angular.module('MaintenanceCtrl', [])
+angular.module('MaintenanceCtrl', ['ngDialog'])
 
-	.controller('MaintenanceController', function($scope, $http, $routeParams, MaintenanceRequest) {
+	.controller('MaintenanceController', function($scope, $http, $routeParams, MaintenanceRequest, ngDialog) {
 		$scope.formData = {};
 
         $scope.createMaintenanceRequest = function() {
         	$scope.formData.apartmentId = $routeParams.apartmentId;
             MaintenanceRequest.create($scope.formData)
                 .success(function(data) {
-                    console.log(data);
+                    ngDialog.open({
+                        template: '<p>my template</p>',
+                        plain: true,
+                        className: 'ngdialog-theme-default'
+                    });
+                }).error(function(data) {
+                    alert(':(');
                 });
         };
 

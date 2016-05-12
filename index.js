df1c374d65d157d006cba08231e79b0364f2fb2c
Tayane Fernandes <tayane.rmf@gmail.com>
2016-05-12 13:42:29 -0300

    Adding webpack

---
+++ index.js
@@ -0,0 +1,16 @@
+require('script!./public/libs/angular/angular.min.js');
+
+require('script!./public/libs/angular-route/angular-route.min.js');
+require('script!./public/libs/angular-sanitize/angular-sanitize.min.js');
+require('script!./public/libs/ngDialog/js/ngDialog.min.js');
+
+require('script!./public/js/app.js');
+require('script!./public/js/appRoutes.js');
+require('script!./public/js/controllers/ApartmentAdminCtrl.js');
+require('script!./public/js/controllers/ApartmentListCtrl.js');
+require('script!./public/js/controllers/ApartmentEditCtrl.js');
+require('script!./public/js/controllers/MaintenanceCtrl.js');
+require('script!./public/js/controllers/MaintenanceListCtrl.js');
+require('script!./public/js/controllers/MaintenanceEditCtrl.js');
+require('script!./public/js/services/MaintenanceRequestService.js');
+require('script!./public/js/services/ApartmentService.js');
